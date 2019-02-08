94d6779dcc80f5f14d1b4c6977a9d2b9c74fa5e0
rwoo <rwoo@bistel.com>
2019-02-08 15:55:33 +0900

    #69-parameter-for-table-name ``` deleted:    05_Tmp/05_sys_stdout_write_with_string_format.md deleted:    05_Tmp/06_datetime_now_strftime.ipynb renamed:    05_Tmp/01_dictionary_in_dictionary.py -> 05_packages/01_collections/01_defaultdict/01_dictionary_in_dictionary.py new file:   05_packages/02_datetime/01_datetime_now_strftime.py renamed:    05_Tmp/02_gzip_write_read.md -> 05_packages/03_gzip/01_gzip_write_read.py renamed:    05_Tmp/03_create_and_visualize_data_approximating_the_cubic_equation.py -> 05_packages/04_matplotlib/01_create_and_visualize_data/01_Approximating_to_user_defiend_equation/01_approximating_to_user_defined_equation.py new file:   05_packages/04_matplotlib/01_create_and_visualize_data/01_Approximating_to_user_defiend_equation/02_user_defined_equation_scatter_plot.png renamed:    05_Tmp/04_create_and_visualize_data_approximation_the_k-means.py -> 05_packages/04_matplotlib/01_create_and_visualize_data/02_Approximating_to_kmeans/01_approximating_to_kmeans.py new file:   05_packages/04_matplotlib/01_create_and_visualize_data/02_Approximating_to_kmeans/02_kmean_scatter_plot.png renamed:    05_Tmp/06_float_to_str_with_a_fixed_number_of_digits.ipynb -> 05_packages/05_no_package/01_float_to_str_with_a_fixed_number_of_digits.ipynb new file:   05_packages/06_psycopg2/01_replace_string_with_dictionary.py new file:   05_packages/07_sys/01_sys_stdout_write_with_string_format.py new file:   05_packages/reNumberFolder.sh new file:   99_Utility/04_modify_number_of_head_on_README.sh modified:   99_Utility/change_A_to_B.txt modified:   README.md ```

---
+++ 05_packages/02_datetime/01_datetime_now_strftime.py
@@ -0,0 +1,5 @@
+import datetime
+
+time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+print(time) # 2019-02-06 12:16:06.712922
+
--- 05_packages/04_matplotlib/01_create_and_visualize_data/01_Approximating_to_user_defiend_equation/01_approximating_to_user_defined_equation.py
+++ 05_packages/04_matplotlib/01_create_and_visualize_data/01_Approximating_to_user_defiend_equation/01_approximating_to_user_defined_equation.py
@@ -1,5 +1,5 @@
 from matplotlib import pyplot
-from pandas import DataFrame
+import pandas as pd
 import numpy as np
 
 # generate target line data
@@ -7,7 +7,7 @@ data_size = 1000
 x = np.arange(data_size)
 delta = np.random.uniform(-300000, 700000000, x.size)
 y = 0.9 * x + x * x * x + delta
-df_y = DataFrame(y)
+df_y = pd.DataFrame(y)
 #df_y2 = df_y.apply(lambda x: (x - np.mean(x)) / (np.max(x) - np.min(x)))
 df_y2 = df_y.apply(lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)))
 y2 = df_y2.values.ravel()
@@ -27,15 +27,16 @@ for i in range(len(data_label)):
             data_label[i] = 2
         else:
             data_label[i] = 0
+for i in range(data_size):
+    print("lot" + str(1000 + i), "NA", data[i][0], data[i][1], data_label[i], sep='\t')
 
 # scatter plot, dots colored by class value
-df = DataFrame(dict(x=data[:, 0], y=data[:, 1], label=data_label))
+df = pd.DataFrame(dict(x=data[:, 0], y=data[:, 1], label=data_label))
 colors = {0:'red', 1:'blue', 2:'green'}
 fig, ax = pyplot.subplots()
 grouped = df.groupby('label')
 for key, group in grouped:
     group.plot(ax=ax, kind='scatter', s=4, x='x', y='y', label=key, color=colors[key])
-pyplot.show()
-
-for i in range(data_size):
-    print("lot" + str(1000 + i), "NA", data[i][0], data[i][1], data_label[i], sep='\t')
+#pyplot.show()
+pyplot.savefig(fname='02_user_defined_equation_scatter_plot.png')
+pyplot.close()
