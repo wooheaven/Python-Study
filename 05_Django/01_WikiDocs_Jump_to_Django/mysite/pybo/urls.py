20fd8fe1aa60dc633494a8e918d1568e537a4055
WooRung <wooheaven79@gmail.com>
2021-11-27 10:45:40 +0900

    #254-wiki-docs-jump-to-django-8-model-answer ``` modified:   05_Django/01_WikiDocs_Jump_to_Django/mysite/pybo/urls.py modified:   05_Django/01_WikiDocs_Jump_to_Django/mysite/pybo/views.py modified:   05_Django/01_WikiDocs_Jump_to_Django/mysite/templates/pybo/question_detail.html ```

--- 05_Django/01_WikiDocs_Jump_to_Django/mysite/pybo/urls.py
+++ 05_Django/01_WikiDocs_Jump_to_Django/mysite/pybo/urls.py
@@ -6,4 +6,5 @@ app_name = 'pybo'
 urlpatterns = [
     path('', views.index),
     path('<int:question_id>/', views.detail, name='detail'),
+    path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
 ]
