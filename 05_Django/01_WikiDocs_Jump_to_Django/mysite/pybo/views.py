755985d7b5546972fbc776deb0a4efd68e7f3317
WooRung <wooheaven79@gmail.com>
2021-12-01 22:58:46 +0900

    #258-wiki-docs-jump-to-django-12-save-question ``` new file:   05_Django/01_WikiDocs_Jump_to_Django/mysite/pybo/forms.py modified:   05_Django/01_WikiDocs_Jump_to_Django/mysite/pybo/urls.py modified:   05_Django/01_WikiDocs_Jump_to_Django/mysite/pybo/views.py new file:   05_Django/01_WikiDocs_Jump_to_Django/mysite/templates/pybo/question_form.html modified:   05_Django/01_WikiDocs_Jump_to_Django/mysite/templates/pybo/question_list.html ```

--- 05_Django/01_WikiDocs_Jump_to_Django/mysite/pybo/views.py
+++ 05_Django/01_WikiDocs_Jump_to_Django/mysite/pybo/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+
+from .forms import QuestionForm
 from .models import Question, Answer
 
 
@@ -21,3 +23,17 @@ def answer_create(request, question_id):
     answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
     answer.save()
     return redirect('pybo:detail', question_id=question_id)
+
+
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {"form": form}
+    return render(request, 'pybo/question_form.html', context)
